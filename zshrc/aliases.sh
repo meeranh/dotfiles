b825b4a35b2cd725469c5a1ae8916828109f50aa
meeranh <themeeranhassan@gmail.com>
2024-02-25 15:26:56 +0530

    Modularized zshrc

---
+++ zshrc/aliases.sh
@@ -0,0 +1,10 @@
+# All my aliases
+alias vim="nvim"
+alias nv="nvim"
+alias v="nvim"
+alias px="proxychains"
+alias nf="neofetch"
+alias s="sway"
+alias l="ls --color=auto"
+alias dnd="killall dunst"
+alias f="fd . | fzf --preview 'bat --style=numbers --color=always {} 2>/dev/null' | xargs -r nvim"
---
+++ zshrc/exports.sh
@@ -0,0 +1,18 @@
+# System environment variables
+export PATH="$HOME/.local/bin:$PATH"
+export GTK_THEME=Materia:dark
+export GDK_DPI_SCALE=1.5
+export PATH=$PATH:/home/neo/.config/scripts
+export EDITOR=nvim
+export PATH=$PATH:/usr/lib
+export QT_SCALE_FACTOR=1.5
+
+# Firefox environment variables
+export MOZ_ENABLE_WAYLAND=1
+
+# Generated for pdtm. Do not edit.
+export PATH=$PATH:/home/neo/.pdtm/go/bin
+
+# Changing GOPATH
+export GOPATH=$HOME/.go
+

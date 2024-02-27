3ddb10d4929c5f6e036fc7c34ccf2100aa838dd8
meeranh <themeeranhassan@gmail.com>
2024-02-27 20:21:08 +0530

    Added alias for Zathura

--- zshrc/aliases.sh
+++ zshrc/aliases.sh
@@ -8,6 +8,7 @@ alias s="sway"
 alias l="ls --color=auto"
 alias dnd="killall dunst"
 alias f="fd . | fzf --preview 'bat --style=numbers --color=always {} 2>/dev/null' | xargs -r nvim"
+alias pdf=zathura
 
 # Setting up Zoxide
 alias cd=z

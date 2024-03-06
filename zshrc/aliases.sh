3e33a99613806ce2fe9160a2e8ba6b4d84a65e18
meeranh <themeeranhassan@gmail.com>
2024-03-07 05:04:36 +0530

    Created alias for Hyprland

--- zshrc/aliases.sh
+++ zshrc/aliases.sh
@@ -5,6 +5,7 @@ alias v="nvim"
 alias px="proxychains"
 alias nf="neofetch"
 alias s="sway"
+alias h=Hyprland
 alias l="ls --color=auto"
 alias dnd="killall dunst"
 alias f="fd . | fzf --preview 'bat --style=numbers --color=always {} 2>/dev/null' | xargs -r nvim"

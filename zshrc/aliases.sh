8880833804775c468c65bff1126900d79c490865
meeranh <themeeranhassan@gmail.com>
2024-03-06 19:46:02 +0530

    Added alias to img

--- zshrc/aliases.sh
+++ zshrc/aliases.sh
@@ -9,6 +9,7 @@ alias l="ls --color=auto"
 alias dnd="killall dunst"
 alias f="fd . | fzf --preview 'bat --style=numbers --color=always {} 2>/dev/null' | xargs -r nvim"
 alias pdf=zathura
+alias img=imv
 
 # Setting up Zoxide
 alias cd=z

5c22b90d3def70239b20234b2a71ec013e52c028
meeranh <themeeranhassan@gmail.com>
2024-03-07 20:17:35 +0530

    Added venv creation and activation scripts

--- zshrc/aliases.sh
+++ zshrc/aliases.sh
@@ -12,5 +12,9 @@ alias f="fd . | fzf --preview 'bat --style=numbers --color=always {} 2>/dev/null
 alias pdf=zathura
 alias img=imv
 
+# Python Venv
+alias vc="python3 -m venv venv"
+alias va="source venv/bin/activate"
+
 # Setting up Zoxide
 alias cd=z

641d84a82d9c8705a95fa3ec8e1ec9b2eac74b84
Meeran Hassan <102342274+meeranh@users.noreply.github.com>
2025-01-15 17:04:31 +0530

    chore: added docker and docker-compose

--- install.sh
+++ install.sh
@@ -9,7 +9,8 @@ sudo pacman --noconfirm -S sway wlroots git base-devel python python-pip make \
     ttf-iosevka-nerd brightnessctl zsh-autosuggestions zsh-syntax-highlighting \
     nemo bpytop swaylock noto-fonts-emoji noto-fonts-extra os-prober grub \
     wlsunset wev ripgrep fzf cowsay tmux networkmanager aws-cli azure-cli \
-    proxychains-ng v2ray github-cli bluez bluez-utils bluetui openvpn cloudflared
+    proxychains-ng v2ray github-cli bluez bluez-utils bluetui openvpn cloudflared \
+    docker docker-compose
 
 # Clone paru-bin from AUR and install it without prompts
 git clone https://aur.archlinux.org/paru-bin.git

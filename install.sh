ff6ec7a14b58cbae98254a51e526fc03b5fb0c2c
meeranh <themeeranhassan@gmail.com>
2025-01-20 17:34:53 +0530

    chore: added package

--- install.sh
+++ install.sh
@@ -5,13 +5,14 @@ sudo pacman -S sway wlroots git base-devel python python-pip make \
     xdg-desktop-portal wlsunset playerctl grim slurp wl-clipboard yazi \
     trash-cli metasploit nmap gnu-netcat luarocks rust nodejs npm pnpm yarn \
     python-i3ipc neofetch zathura xdg-utils bluetui imv mpv zoxide pyenv hurl \
-    nemo bpytop swaylock noto-fonts-emoji noto-fonts-extra os-prober grub \
+    nemo btop swaylock noto-fonts-emoji noto-fonts-extra os-prober grub \
     wlsunset wev ripgrep fzf cowsay tmux networkmanager aws-cli azure-cli \
     proxychains-ng v2ray github-cli bluez bluez-utils bluetui openvpn cloudflared \
     docker docker-compose wireplumber less tree ttf-iosevka-nerd brightnessctl fd \
     tldr locate go python-pipx glow ltrace cutter radare2 rz-ghidra r2ghidra \
     libvirt qemu-full qemu-img virt-install virt-manager virt-viewer man swaylock \
-		edk2-ovmf dnsmasq swtpm guestfs-tools libosinfo tuned go adwaita-icon-theme
+		edk2-ovmf dnsmasq swtpm guestfs-tools libosinfo tuned go adwaita-icon-theme \
+		lzip
 
 # Clone paru-bin from AUR and install it
 git clone https://aur.archlinux.org/paru-bin.git
@@ -22,7 +23,7 @@ rm -rf paru-bin
 
 # Install additional packages via paru
 paru -S yambar caido-cli burpsuite zen-browser-bin xcp virtualfish waydroid \
-	python-pyclip subfinder-bin
+	python-pyclip subfinder httpx
 
 # Clone dotfiles repository and set up .zshrc
 rm -rf ~/.config

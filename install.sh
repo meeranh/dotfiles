f5c15b7e4960737cda506695366bf75010826d0e
meeranh <themeeranhassan@gmail.com>
2026-04-06 19:16:06 +0530

    feat: added fastfetch

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ sudo pacman -S sway wlroots0.20 git base-devel python python-pip make \
 		edk2-ovmf dnsmasq swtpm guestfs-tools libosinfo tuned adwaita-icon-theme \
 		lzip csvlens bat jq htop gitleaks bind kdeconnect sshfs python-nautilus \
 		net-tools sqlmap xdg-utils ghidra wireshark-qt perl-image-exiftool binwalk \
-		imagemagick hydra smbclient remmina freerdp hashcat pocl 7zip waybar inotify-tools
+		imagemagick hydra smbclient remmina freerdp hashcat pocl 7zip waybar inotify-tools fastfetch
 
 # Clone paru-bin from AUR and install it
 git clone https://aur.archlinux.org/paru-bin.git

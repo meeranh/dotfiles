f3885d66d92c8678f6e635ace793715c81002ca2
meeranh <themeeranhassan@gmail.com>
2026-06-25 13:44:11 +0530

    fix: fixed wrong github-copilot-cli-bin name

--- install.sh
+++ install.sh
@@ -30,7 +30,7 @@ rm -rf paru
 paru -S caido-cli burpsuite zen-browser-bin xcp waydroid \
 	python-pyclip subfinder httpx feroxbuster-bin urlencode netexec \
 	ruby-evil-winrm clockify-cli-bin nerdfetch-git eww android-apktool-bin \
-	nuclei-bin dnsx-bin xrdp xorgxrdp pipewire-module-xrdp claude-code copilot
+	nuclei-bin dnsx-bin xrdp xorgxrdp pipewire-module-xrdp claude-code github-copilot-cli-bin
 
 # Clone dotfiles repository and set up .zshrc
 rm -rf ~/.config
